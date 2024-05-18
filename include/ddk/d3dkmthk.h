8810dcefec81be8e457affba9c64507ed93fb85f
Kron4ek <kron4ek@gmail.com>
2024-05-18 14:52:38 +0300

    Fix compilation

--- include/ddk/d3dkmthk.h
+++ include/ddk/d3dkmthk.h
@@ -773,22 +773,6 @@ typedef struct _D3DKMT_ENUMADAPTERS2
   D3DKMT_ADAPTERINFO *pAdapters;
 } D3DKMT_ENUMADAPTERS2;
 
-typedef struct _D3DKMT_WDDM_2_7_CAPS
-{
-  union
-  {
-    struct
-    {
-      UINT HwSchSupported               :  1;
-      UINT HwSchEnabled                 :  1;
-      UINT HwSchEnabledByDefault        :  1;
-      UINT IndependentVidPnVSyncControl :  1;
-      UINT Reserved                     : 28;
-    };
-    UINT Value;
-  };
-} D3DKMT_WDDM_2_7_CAPS;
-
 #ifdef __cplusplus
 extern "C"
 {
